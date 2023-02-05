9c0b3641f9b6270d50ad24c4a0ae7ec296b2513d
andrewvitrenko <avitrenko2004@gmail.com>
2023-02-05 14:27:07 +0100

    feat(ingredients): created `IngredientsService`, added types

---
+++ src/types/ingredients.ts
@@ -0,0 +1,62 @@
+enum MeasureType {
+  KILOGRAMMS,
+  LITERS,
+  GRAMMS,
+  MILLILITERS,
+  PIECES,
+  BUNCH,
+  TABLE_SPOON,
+  TEA_SPOON,
+  CUP,
+  PINCH,
+  CLOVE,
+}
+
+export interface Measure {
+  measure: MeasureType;
+  toStandard: number;
+}
+
+export interface Ingredient {
+  id: string;
+  name: string;
+  standard: MeasureType;
+  categoryId?: string;
+  category?: {
+    id: string;
+    name: string;
+  };
+  measures?: Measure[];
+}
+
+export interface GetAllQuery {
+  search?: string;
+  measures?: boolean;
+  category?: boolean;
+}
+
+export interface GetAllResponse {
+  ingredients: Ingredient[];
+}
+
+export interface GetOneQuery {
+  measures?: boolean;
+  category?: boolean;
+}
+
+export type GetOneResponse = Ingredient;
+
+export interface CreateIngredientPayload {
+  name: string;
+  standard: MeasureType;
+  categoryId: string;
+  measures?: Measure[];
+}
+
+export type CreateIngredientResponse = Ingredient;
+
+export interface EditIngredientPayload {
+  name?: string;
+  standard?: MeasureType;
+  categoryId?: string;
+}
