60e121e68593d2d9c0d8351d116987276e51bd73
andrewvitrenko <avitrenko2004@gmail.com>
2023-02-06 17:53:05 +0100

    fix(drawer): removed unused import

--- src/components/common/header/components/drawer/drawer.tsx
+++ src/components/common/header/components/drawer/drawer.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import LogoutIcon from '@mui/icons-material/Logout';
