4fea5c6b96570ecab9381e2c590eb81ca3352475
OleksandrHumeniuk <oleksandrhumeniuk2@gmail.com>
2023-02-07 20:16:47 +0200

    feat: added redirect for unauthorized users.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,32 @@
+import { useEffect } from 'react';
 import { Provider } from 'react-redux';
+import { Router } from '@mui/icons-material';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 import Header from '@/components/common/header';
 import Toast from '@/components/common/toast';
+import { useIsLoggedIn } from '@/hooks';
 import store from '@/redux';
+import { ROUTES } from '@/types/common';
 
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const isLoggedIn = useIsLoggedIn();
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log('IS LOGGED IN: ' + isLoggedIn);
+    if (
+      !isLoggedIn &&
+      !router.asPath.includes('signup') &&
+      !router.asPath.includes('login')
+    ) {
+      router.push(ROUTES.LOGIN);
+    }
+  }, [isLoggedIn, router]);
+
   return (
     <Provider store={store}>
       <Header />
